c3f6cec37b29267f9d816b216a6445f33a471071
NurikS <nurbek.saidnassim@gmail.com>
2020-10-10 20:36:40 +0100

    added conv layer script.

---
+++ src/layers/conv.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('../src/helpers')
+from im2col import im2col
+import numpy as np
+
+def convolve2d(X, kernel):
+    convolved = np.dot(kernel.flatten(), im2col(X, 3))
+    shapes = convolved.shape[0]
+    return convolved.reshape(int(np.sqrt(shapes)), int(np.sqrt(shapes)))
